67b3541f1a62fc83369966a3c510829a6fbd6a56
Maciej Rozpedek <maciej.rozpedek@gmail.com>
2026-03-07 22:32:02 +0100

    init project

---
+++ src/main/java/com/flatshareteam/flatsharebackend/FlatshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flatshareteam.flatsharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatshareBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FlatshareBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flatshareteam/flatsharebackend/FlatshareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flatshareteam.flatsharebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatshareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
