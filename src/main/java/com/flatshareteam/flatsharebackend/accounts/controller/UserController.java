6cfb29b2b1655997a7099c9e54b1e548fbc213ad
Bartosz Stępień <bartoszstepien01@interia.pl>
2026-03-22 23:32:49 +0100

    feat: [KAN-11] Implement user registration.

--- src/main/java/com/flatshareteam/flatsharebackend/accounts/controller/UserController.java
+++ src/main/java/com/flatshareteam/flatsharebackend/accounts/controller/UserController.java
@@ -3,10 +3,15 @@ package com.flatshareteam.flatsharebackend.accounts.controller;
 import com.flatshareteam.flatsharebackend.accounts.dto.RegistrationRequest;
 import com.flatshareteam.flatsharebackend.accounts.dto.RegistrationResponse;
 import com.flatshareteam.flatsharebackend.accounts.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -18,9 +23,16 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/register")
-    public RegistrationResponse register(@RequestBody RegistrationRequest request) {
-        return userService.createUser(request);
-    }
+    @PostMapping
+    public ResponseEntity<RegistrationResponse> register(@Valid @RequestBody RegistrationRequest request) {
+        RegistrationResponse response = userService.createUser(request);
 
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/users/{id}")
+                .buildAndExpand(response.user().id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
 }
