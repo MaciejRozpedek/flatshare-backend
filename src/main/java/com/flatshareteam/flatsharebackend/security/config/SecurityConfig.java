e8c025969ebda1cc793855200c545bf48b0b8a5d
Stawicki2137 <105315201+Stawicki2137@users.noreply.github.com>
2026-03-23 10:00:35 +0100

    fix
    podczas szybkiego rozwiazywania pojawil sie nadmiarowy nawias

--- src/main/java/com/flatshareteam/flatsharebackend/security/config/SecurityConfig.java
+++ src/main/java/com/flatshareteam/flatsharebackend/security/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
@@ -62,4 +60,3 @@ public class SecurityConfig {
     }
 }
 
-}
